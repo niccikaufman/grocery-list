dfe9d0d824d1d13daf3dd04e3a11c12b99e44e0b
Nicci Kaufman <niccikaufman@gmail.com>
2021-04-09 14:16:14 -0700

    fix ability to inject array elements into DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,20 @@
 $(document).ready(function(){
-  let typedValueArray = [];
-  let capitalizedItemsArray = [];
+  const typedValueArray = [];
   const typedValueElement = document.getElementById('typed-value');
 
-  //push inputted typed value into array after each click
+  //push typed value into array after each click
   $('#add-item').on('click', function(event) {
     event.preventDefault();
-    $('#typed-value').each(function() {
-      typedValueArray.push(typedValueElement.value);
-      //clear input field and log array to console
-      typedValueElement.value = '';
-      console.log(typedValueArray);
-    });
-    //capitalize each element and add to new array
-    typedValueArray.forEach(function(element) {
-    let elementToUpperCase = element.toUpperCase();
-    capitalizedItemsArray.push(elementToUpperCase);
-    });
-  });
-  //make every element uppercase and push to new array
-  $('#compile-list').on('click', function(event) {
-    //list capitalized array elements on page
-    for (let i = 0; i < capitalizedItemsArray.length -1; i++) {
-      $('#form-output').text(`Item: ${capitalizedItemsArray[i]}, `);
-    };
+    typedValueArray.push(typedValueElement.value);
+    console.log(typedValueArray);
+    //clear input field
+    typedValueElement.value = '';
+    let text = "";
+    //append array elements to a list
+    for (let i = 0; i < typedValueArray.length; i++) {
+      text += "<li>" + typedValueArray[i] + "</li>";
+      }
+    document.getElementById("form-output").innerHTML = text;
     $('#form-output').show();
-
+    });
   });
-})
-
-
